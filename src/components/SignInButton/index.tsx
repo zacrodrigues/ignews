2020bbb67f4f613fc9f4d9115cd597292f610d2e
Romanoff <romanoff@goarca.club>
2022-10-18 14:01:17 -0300

    Componentes e páginas

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,27 @@
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+    >
+      <FaGithub color='#04d361' />
+      Zac Rodrigues
+      <FiX color='#737380' />
+    </button>
+  ) : (
+    <button
+      type="button"
+      className={styles.signInButton}
+    >
+      <FaGithub color='#eba417' />
+      Sign in with GitHub
+    </button>
+  );
+}
